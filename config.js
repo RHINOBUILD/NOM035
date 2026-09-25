30dcaa8be3ad629853d4740e6a039ab1b372f087
RHINOBUILD <fernandocarrillor990@gmail.com>
2026-09-24 20:24:52 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
  * Pegue aquí la URL de la aplicación web de Apps Script (termina en /exec).
  * ENCUESTA_URL es opcional: si lo deja vacío, el panel usa encuesta.html de la misma carpeta. */
 window.NOM035_CONFIG = {
-  API_URL: '',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwedh-6kgOR-SK82aBH8B6RVjGHJpePEQTdcKLDB_Ifbx1YJh81N-2gvkFT2LBalbOoWw/exec',
   ENCUESTA_URL: ''
 };
